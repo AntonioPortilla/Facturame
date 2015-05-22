e1acdad378de95d5272ad93669791641d38ab791
Julio Laveriano en SITATEL <julioantonio.lp@gmail.com>
2015-05-22 18:37:56 -0500

    Facturamela WP avanzado

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      pub: 'public',
+      docs: 'docs/prueba'
+    },
+    concat: {
+      options: {
+          stripBanners: true
+      },
+      dist: {
+          src: 'dev/css/*.css',
+          dest: 'prod/css/styles.css'
+      }
+    },
+    cssmin: {
+      compress: {
+        files: {
+          "public/css/styles.min.css": 'prod/css/styles.css'
+        }
+      }
+    }, 
+    uglify: {
+      core: {
+        files: {
+          'public/js/scripts.min.js': [
+          'dev/js/scripts.js',
+          'dev/js/jquery.sabecarousel.js'
+          ]
+        }
+      }
+    },
+    copy: {
+      fonts: {
+        expand: true,
+        src: ['fonts/*', 'images/*', 'tpl/*'],
+        dest: 'public/'
+      },
+      docs: {
+        expand: true,
+        cwd: 'public/',
+        src: [
+          '**/*'
+        ],
+        dest: 'docs/public'
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'dev/js/scripts.js']
+    },    
+    exec: {
+      npmUpdate: {
+        command: 'npm update'
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.'
+        }
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  // Default task(s).
+  grunt.registerTask('minify', ['cssmin', 'uglify']);  
+  grunt.registerTask('public', ['copy:docs', 'clean', 'concat', 'minify', 'copy:fonts']);
+
+};
